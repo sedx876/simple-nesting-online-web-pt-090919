43fa4217b5111b3df514abf4fd626ce56f074dff
Sharon Watkins <swatkins876@gmail.com>
2019-09-16 12:19:22 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash.to_s[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,7 +82,10 @@ def adding_matz
         }
      }
 
-    
+  programmer_hash << :yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }  
 end
 
 def changing_alan
@@ -102,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+    programmer_hash[:alan_kay][:known_for] = "GUI" 
 end
 
 def adding_to_dennis
@@ -121,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
